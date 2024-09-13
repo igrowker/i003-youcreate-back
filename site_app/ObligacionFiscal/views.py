4bb5324dcf97e691f828745c10a11f9845adfbea
Lopez Victor Fernando <victorfernandolopez1988@gmail.com>
2024-09-13 11:14:14 -0300

    Agregue ObligacionFiscalViewSet para el manejo de obligaciones fiscales.

--- site_app/ObligacionFiscal/views.py
+++ site_app/ObligacionFiscal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import ObligacionFiscal
+from .serializers import ObligacionFiscalSerializer
+
+class ObligacionFiscalViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ObligacionFiscal.objects.all()
+    serializer_class = ObligacionFiscalSerializer
