1814caacdba9e2211a939c79eaa0d2a8e5592a2f
Lopez Victor Fernando <victorfernandolopez1988@gmail.com>
2024-09-13 08:26:16 -0300

    feat(app): create new django app ObligacionFiscal

--- site_app/site_app/settings.py
+++ site_app/site_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ObligacionFiscal',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
