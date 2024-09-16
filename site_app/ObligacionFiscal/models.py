a2624a9ffe1db1b78306fed440b4ab08a1e080f3
Lopez Victor Fernando <victorfernandolopez1988@gmail.com>
2024-09-16 12:25:13 -0300

    fix modelo ObligacionFiscal

--- site_app/ObligacionFiscal/models.py
+++ site_app/ObligacionFiscal/models.py
@@ -1,13 +1,24 @@
 from django.db import models
-from Usuario.models import Usuario
+from Usuario.models import Usuario # Importamos el modelo de usuarios de Django
 
 
-# Create your models here.
+# Modelo ObligacionFiscal, que almacenará la información sobre los impuestos a pagar
 class ObligacionFiscal(models.Model):
-    tipo_impuesto = models.CharField(max_length=255)
-    monto_a_pagar = models.DecimalField(max_digits=10, decimal_places=2)
-    fecha_vencimiento = models.DateField()
-    usuario_id = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    tipo_impuesto = models.CharField(max_length=255) # Tipo de impuesto (ej: IVA, Ganancias)
+    monto_a_pagar = models.DecimalField(max_digits=10, decimal_places=2)# Monto a pagar
+    fecha_vencimiento = models.DateField()# Fecha límite para el pago del impuesto
+    usuario_id = models.ForeignKey(Usuario, on_delete=models.CASCADE)# Relación con el usuario
 
     def __str__(self):
         return f"{self.tipo_impuesto} - {self.monto_a_pagar} - {self.fecha_vencimiento}"
+
+"""
+Comentarios:
+
+models.CharField: Define un campo de texto.
+models.DecimalField: Almacena un número decimal con un máximo de 10 dígitos, de los cuales 2 serán decimales.
+models.DateField: Almacena una fecha.
+models.ForeignKey: Crea una relación entre ObligacionFiscal y User. Cuando un usuario sea eliminado, todas sus obligaciones fiscales también serán eliminadas (on_delete=models.CASCADE).
+"""
+def __str__(self):
+        return f"{self.tipo_impuesto} - {self.monto_a_pagar}"
