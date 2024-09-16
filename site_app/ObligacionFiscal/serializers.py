2ab77b7935b9e5ee7f2f9bdf57e857b9bc14b88b
Lopez Victor Fernando <victorfernandolopez1988@gmail.com>
2024-09-16 12:27:26 -0300

    fix serializer.py de ObligacionFiscal

--- site_app/ObligacionFiscal/serializers.py
+++ site_app/ObligacionFiscal/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from .models import ObligacionFiscal
 
-
+# Serializador para el modelo ObligacionFiscal
 class ObligacionFiscalSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ObligacionFiscal
-        fields = "__all__"
+        model = ObligacionFiscal # Modelo que vamos a serializar
+        fields = ['tipo_impuesto', 'monto_a_pagar', 'fecha_vencimiento']  # Campos que incluimos en la respuesta JSON
