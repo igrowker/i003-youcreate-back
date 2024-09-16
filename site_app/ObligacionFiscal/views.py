2893103b599848cf7bcc31da18ff52465da767cd
Lopez Victor Fernando <victorfernandolopez1988@gmail.com>
2024-09-16 12:41:26 -0300

    fix views.py de ObligacionFiscal

--- site_app/ObligacionFiscal/views.py
+++ site_app/ObligacionFiscal/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import ObligacionFiscal
 from .serializers import ObligacionFiscalSerializer
 
+# Controlador para manejar las obligaciones fiscales
+class ObligacionesFiscalesController(APIView):
+    permission_classes = [IsAuthenticated]  # Solo usuarios autenticados pueden acceder
 
-class ObligacionFiscalViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = ObligacionFiscal.objects.all()
-    serializer_class = ObligacionFiscalSerializer
+    def get(self, request):
+        usuario = request.user  # Obtenemos el usuario autenticado
+        obligaciones = ObligacionFiscal.objects.filter(usuario=usuario)  # Filtramos por el usuario
+        serializer = ObligacionFiscalSerializer(obligaciones, many=True)  # Serializamos las obligaciones
+        return Response(serializer.data)  # Devolvemos las obligaciones en formato JSON
