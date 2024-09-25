d653fd20606c07dd29c107530f0c3bb09fe040c5
Lopez Victor Fernando <victorfernandolopez1988@gmail.com>
2024-09-25 12:01:26 -0300

    agregado del campo estado_pago, email_automatico en ObligacionFiscal

--- site_app/ObligacionFiscal/models.py
+++ site_app/ObligacionFiscal/models.py
@@ -9,6 +9,8 @@ class ObligacionFiscal(models.Model):
     monto_a_pagar = models.DecimalField(max_digits=10, decimal_places=2)# Monto a pagar
     fecha_vencimiento = models.DateField()# Fecha límite para el pago del impuesto
     usuario_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)# Relación con el usuario
+    estado_pago = models.BooleanField(default=False) # Indica si el impuesto ha sido pagado o no
+    email_automatico = models.BooleanField(default=False) # Indica si se enviará un email automático al usuario
 
 def __str__(self):
         return f"{self.tipo_impuesto} - {self.monto_a_pagar}"
