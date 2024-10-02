5bbef83ef0220ecb057f7548c22e057889b022f9
André Candeloro <candeloro95@gmail.com>
2024-10-02 21:35:16 +0200

    fix(Ingreso, settings): Fix para imports en Ingreso.models y import de celery

--- site_app/Ingreso/models.py
+++ site_app/Ingreso/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from Usuario.models import Usuario
+from Usuario.models import CustomUser
 #El modelo nos permite representar la entidad, sus atributos y relaciones
 class Ingreso(models.Model):
     id = models.AutoField(primary_key=True)
     monto = models.DecimalField(max_digits=10, decimal_places=2)
     origen = models.CharField(max_length=255)
     fecha = models.DateField()
-    usuario_id = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-
+    usuario_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
