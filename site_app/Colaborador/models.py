505af41dafc270cbc13d5f7acbe1c3087873c555
André Candeloro <candeloro95@gmail.com>
2024-10-13 00:42:58 +0200

    Mid-Fix: Cambios a los modelos de Colaborador/pagos

--- site_app/Colaborador/models.py
+++ site_app/Colaborador/models.py
@@ -6,8 +6,8 @@ from Usuario.models import CustomUser
 class Colaborador(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=255)
-    servicio = models.CharField(max_length=255)
-    monto = models.DecimalField(max_digits=25, decimal_places=2)
+    servicio = models.CharField(max_length=255, required=False)
+    monto = models.DecimalField(max_digits=50, decimal_places=2, required=False)
     usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
